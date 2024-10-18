bac4336d288131869c2e3e24a84564f8501c6d5c
MomusWinner <areg06.06@mail.ru>
2024-10-18 14:33:52 +0300

    add new shapes

--- shapes/register_shapes.lua
+++ shapes/register_shapes.lua
@@ -10,7 +10,27 @@ local shapes = {
         name = "Треугольник",
         path_to_scheme = "/shapes/triangle/scheme.json",
         projection_id = "triangle",
-    }
+    },
+    {
+        name = "Прямой триугольник",
+        path_to_scheme = "/shapes/straight_triangle/scheme.json",
+        projection_id = "straight_triangle",
+    },
+    {
+        name = "Ромб",
+        path_to_scheme = "/shapes/rhomb/scheme.json",
+        projection_id = "rhomb",
+    },
+    {
+        name = "Птичка",
+        path_to_scheme = "/shapes/bird/scheme.json",
+        projection_id = "bird",
+    },
+    {
+        name = "Странный прямоугольник",
+        path_to_scheme = "/shapes/strange_quadrangle/scheme.json",
+        projection_id = "strange_quadrangle",
+    },
 }
 
 function M.get_shapes()
